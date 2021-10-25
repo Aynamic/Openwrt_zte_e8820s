c08f43aa28709e3d4b8812a04d9b1c020944816b
neabei <67257687+neabei@users.noreply.github.com>
2021-10-25 16:42:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,8 +14,9 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Add zte_e8820s part in mt7621.mk and e8820s dts file
+echo "pwd"
 pwd
+echo "ls ../"
 ls ../
-ls $GITHUB_WORKSPACE/
 cat ../mt7621.mk_zte_e8820s >> target/linux/ramips/image/mt7621.mk
 cp ../mt7621_zte_e8820s.dts  target/linux/ramips/dts/mt7621_zte_e8820s.dts
