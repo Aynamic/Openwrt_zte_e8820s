f235f1e2c4c2c9b229aba42ee902e0368033452d
neabei <67257687+neabei@users.noreply.github.com>
2021-10-28 10:41:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,24 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
+# 修改主机名字，把E8820S修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i 's/OpenWrt/E8820S/g' package/base-files/files/bin/config_generate
+
+# 修改开源驱动wifi名称
+sed -i 's/OpenWrt/ZTE-E8820S-$/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 修改闭源驱动2G wifi名称
+sed -i 's/OpenWrt_2G/ZTE-E8820S/g' package/lean/mt/drivers/mt_wifi/files/mt7603.dat
+
+# 修改闭源驱动5G wifi名称
+sed -i 's/OpenWrt_5G/ZTE-E8820S-5G/g' package/lean/mt/drivers/mt_wifi/files/mt7612.dat
+
+# 更改时区
+sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+#修正连接数
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
+
 # Add zte_e8820s part in mt7621.mk and e8820s dts file
 cd ../
 cat mt7621.mk_zte_e8820s >> openwrt/target/linux/ramips/image/mt7621.mk
